2b62baab3f277d1a80155b67625c428638704f70
Rafael Santos <rafa.05125@gmail.com>
2023-07-26 00:20:57 -0300

    ApiProperty decorator for DTO's

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsBoolean, IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateUserDto {
+  @ApiProperty({ description: "User email" })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ description: "User full name" })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: "Defines if a user is an admin.",
+    default: false,
+  })
   @IsBoolean()
   admin: boolean;
 }
